d2bc4b63993d322a60f082c5ddcf0c2af2026d6c
marianaG11 <mariana.garcia.16@icloud.com>
2022-09-10 22:54:26 -0500

    add app images and links

--- pages/projects.py
+++ pages/projects.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from PIL import Image
+
+image1 = Image.open("images/memory-game.png")
+image2 = Image.open("images/chicago-eats.png")
+image3 = Image.open("images/quickflix.png")
+image4 = Image.open("images/album-collector.png")
+image5 = Image.open("images/daily.png")
+
+
 
 st.title("Projects")
 
@@ -7,25 +16,29 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(["Memory Game", "Chicago Eats", "QuickFli
 with tab1:
     st.header("Memory Card Game")
     st.write("The memory card game was built using HTML, CSS and JavaScript.")
-    st.write("Launch the game here! ")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    st.write("[Launch the game here!](https://marianag11.github.io/GA-Project-1--Game-/)")
+    st.image(image1, width=450)
 
 with tab2:
     st.header("Chicago Eats")
     st.write("Chicago Eats is a restaurant guide for places to eat. This project was built using JavaScript, CSS, EJS, Node.js, Express, MongoDB and Google Oauth.")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.write("[View the app here!](https://project-2-ga.herokuapp.com)")
+    st.image(image2, width=450)
 
 with tab3:
     st.header("QuickFlix")
     st.write("QuickFlix is an app that brings the best movies of Netflix, Hulu, and Amazon Prime to one place! It is a group project built using Python, Django and Django Authentication.")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.write("[Launch the app!](https://quickflixmovies.herokuapp.com)")
+    st.image(image3, width=450)
     
 with tab4:
     st.header("Album Collector")
     st.write("Album Collector is an application that allows users to collect their favorite albums. This project was built using Python, Django and AWS.")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.write("[start your collection!](https://albumcollection.herokuapp.com/albums/)")
+    st.image(image4, width=450)
     
 with tab5:
     st.header("Daily")
     st.write("Daily is an app where users can track their workouts and upload photos they took that day. This project was built using React, JS, Express.js and JSX.")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.write("[Launch the app!](https://dailyworkouts.herokuapp.com/login)")
+    st.image(image5, width=450)
