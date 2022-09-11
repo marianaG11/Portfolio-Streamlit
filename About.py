3e610620259ef21df3c88b8d27805849e72e38e5
marianaG11 <mariana.garcia.16@icloud.com>
2022-09-10 22:20:18 -0500

    add skills

---
+++ About.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+st.title("About Me")
+st.write("I am currently a Software Engineer Intern at Adobe. Prior to becoming an intern, I attended General Assembly Bootcamp through the Adobe Digital Academy. I graduated college with a Bachelor's of Science in Psychology. I have 6 years of experience in customer service which has allowed me to gain excellent communication skills, attention to detail and problem solving skills. I am passionate about contributing high quality solutions to projects and teams.")
+
+st.subheader("View more on GitHub")
+
+st.write("---")
+st.subheader("Skills")
+
+with st.container():
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.write("Technical:")
+        st.write(
+                """
+                - JavaScript
+                - Python3
+                - Django
+                - React
+                - Express.js
+                - Mongoose
+                - Node.js
+                - MongoDB
+                - PostgreSQL
+                - HTML5
+                - CSS3
+                - VS Code
+                - Git and GitHub
+                """)
+        st.write("Proficient in English and Spanish")
+        
+
+st.subheader("Resume")
+
--- pages/contact_me.py
+++ pages/contact_me.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Contact Me")
 
 st.write("Email: mariana.garcia.16@icloud.com")
-st.write("Connect with me on LinkedIn")
+st.write("[Connect with me on LinkedIn](https://www.linkedin.com/in/marianagarcia1/)")
