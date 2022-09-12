4355cb418b9ef254d86a4536c9a5cc0b97c389c1
marianaG11 <mariana.garcia.16@icloud.com>
2022-09-12 01:08:08 -0500

    addnprofile pic

--- About.py
+++ About.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("About Me")
 st.write("I am currently a Software Engineer Intern at Adobe. Prior to becoming an intern, I attended General Assembly Bootcamp through the Adobe Digital Academy. I graduated college with a Bachelor's of Science in Psychology. I have 6 years of experience in customer service which has allowed me to gain excellent communication skills, attention to detail and problem solving skills. I am passionate about contributing high quality solutions to projects and teams.")
 
-st.subheader("View more on GitHub")
+st.subheader("[View more on GitHub](https://github.com/marianaG11)")
 
 st.write("---")
 st.subheader("Skills")
@@ -29,7 +29,3 @@ with st.container():
                 - Git and GitHub
                 """)
         st.write("Proficient in English and Spanish")
-        
-
-st.subheader("Resume")
-
--- pages/contact_me.py
+++ pages/contact_me.py
@@ -1,6 +1,14 @@
 import streamlit as st
-
+from PIL import Image
+image = Image.open("images/profile-pic.jpeg")
 st.title("Contact Me")
 
-st.write("Email: mariana.garcia.16@icloud.com")
-st.write("[Connect with me on LinkedIn](https://www.linkedin.com/in/marianagarcia1/)")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write("Email: mariana.garcia.16@icloud.com")
+    st.write("[Connect with me on LinkedIn](https://www.linkedin.com/in/marianagarcia1/)")
+    
+    
+with col2:
+    st.image(image, width=350)
